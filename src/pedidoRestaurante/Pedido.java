6a3bd94cd9c5426ccc45c37ea5cf2b213bb6fb8e
Antonio <antoniodawgc@gmail.com>
2026-02-09 00:05:06 +0000

    pedido actualizacion

--- src/pedidoRestaurante/Pedido.java
+++ src/pedidoRestaurante/Pedido.java
@@ -1,59 +1,63 @@
-package pedidoRestaurante;
-
 public class Pedido {
-   private static int contadorPedidos = 1;
-   private int idPedido;
-   private String nombreCliente;
-   private Articulo[] articulos;
-   private int cantidadArticulos;
-   private EstadoPedido estado;
 
-   public Pedido(String var1) {
-      this.idPedido = contadorPedidos++;
-      this.nombreCliente = var1;
-      this.articulos = new Articulo[5];
-      this.cantidadArticulos = 0;
-      this.estado = EstadoPedido.EN_PREPARACION;
-   }
+    private static int contadorPedidos = 1;
 
-   public void agregarArticulo(String var1, int var2, double var3) {
-      if (this.cantidadArticulos >= 5) {
-         System.out.println("No se pueden agregar más de 5 artículos.");
-      } else {
-         this.articulos[this.cantidadArticulos] = new Articulo(var1, var2, var3);
-         ++this.cantidadArticulos;
-      }
-   }
+    private int idPedido;
+    private String nombreCliente;
+    private Articulo[] articulos;
+    private int cantidadArticulos;
+    private EstadoPedido estado;
 
-   public double calcularTotal() {
-      double var1 = 0.0;
+    public Pedido(String nombreCliente) {
+        this.idPedido = contadorPedidos++;
+        this.nombreCliente = nombreCliente;
+        this.articulos = new Articulo[5];
+        this.cantidadArticulos = 0;
+        this.estado = EstadoPedido.EN_PREPARACION;
+    }
 
-      for(int var3 = 0; var3 < this.cantidadArticulos; ++var3) {
-         var1 += this.articulos[var3].getSubtotal();
-      }
+    public void agregarArticulo(String nombre, int cantidad, double precioUnitario) {
+        if (cantidadArticulos >= 5) {
+            System.out.println("No se pueden agregar más de 5 artículos.");
+            return;
+        }
 
-      return var1;
-   }
+        articulos[cantidadArticulos] = new Articulo(nombre, cantidad, precioUnitario);
+        cantidadArticulos++;
+    }
 
-   public void cambiarEstado(EstadoPedido var1) {
-      switch (this.estado) {
-         case EN_PREPARACION:
-            if (var1 != EstadoPedido.LISTO_PARA_ENTREGAR && var1 != EstadoPedido.ENTREGADO) {
-               System.out.println("No se puede retroceder el estado del pedido.");
-            } else {
-               this.estado = var1;
-            }
-            break;
-         case LISTO_PARA_ENTREGAR:
-            if (var1 == EstadoPedido.ENTREGADO) {
-               this.estado = var1;
-            } else {
-               System.out.println("No se puede retroceder el estado del pedido.");
-            }
-            break;
-         case ENTREGADO:
-            System.out.println("No se puede cambiar, el pedido ya fue entregado.");
-      }
+    public double calcularTotal() {
+        double total = 0;
 
-   }
+        for (int i = 0; i < cantidadArticulos; i++) {
+            total += articulos[i].getSubtotal();
+        }
+
+        return total;
+    }
+
+    public void cambiarEstado(EstadoPedido nuevoEstado) {
+        switch (estado) {
+            case EN_PREPARACION:
+                if (nuevoEstado == EstadoPedido.LISTO_PARA_ENTREGAR || nuevoEstado == EstadoPedido.ENTREGADO) {
+                    estado = nuevoEstado;
+                } else {
+                    System.out.println("No se puede retroceder el estado del pedido.");
+                }
+                break;
+
+            case LISTO_PARA_ENTREGAR:
+                if (nuevoEstado == EstadoPedido.ENTREGADO) {
+                    estado = nuevoEstado;
+                } else {
+                    System.out.println("No se puede retroceder el estado del pedido.");
+                }
+                break;
+
+            case ENTREGADO:
+                System.out.println("No se puede cambiar, el pedido ya fue entregado.");
+                break;
+        }
+    }
+
 }
